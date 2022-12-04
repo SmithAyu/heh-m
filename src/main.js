d36aa2cc14835433c5ada5868d7fc075f3b3cbdd
SmithAyu <1079119792@qq.com>
2022-12-04 15:20:00 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import Vant from 'vant'
+import 'vant/lib/index.css'
+//  加载全局样式
+import './styles/index.less'
+createApp(App).use(store).use(router).use(Vant).mount('#app')
