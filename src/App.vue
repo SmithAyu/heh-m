58fb6c4aecd7ee2dbc3a433229f2ddc116481e3e
SmithAyu <1079119792@qq.com>
2022-12-05 11:43:35 +0800

    第二次提交

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <h1>黑马头条</h1>
-    <div><i class="m mdianzan1"></i></div>
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant'
+import 'amfe-flexible'
 import 'vant/lib/index.css'
 //  加载全局样式
 import './styles/index.less'
